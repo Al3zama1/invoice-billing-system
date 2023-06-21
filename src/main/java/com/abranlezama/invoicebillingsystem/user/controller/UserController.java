7f497ca815cab0d4e3900c4bb80ad2ba1ab53915
Abran Lezama <lezama.abran@gmail.com>
2023-06-20 21:59:28 -0700

    Removed unused imports

--- src/main/java/com/abranlezama/invoicebillingsystem/user/controller/UserController.java
+++ src/main/java/com/abranlezama/invoicebillingsystem/user/controller/UserController.java
@@ -1,13 +1,10 @@
 package com.abranlezama.invoicebillingsystem.user.controller;
 
-import com.abranlezama.invoicebillingsystem.user.User;
 import com.abranlezama.invoicebillingsystem.user.dto.RegisterUserRequestDto;
 import com.abranlezama.invoicebillingsystem.user.dto.UserDto;
 import com.abranlezama.invoicebillingsystem.user.service.UserService;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotBlank;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,12 +13,9 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponents;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.time.LocalDateTime;
-import java.util.Map;
-
-import static java.time.LocalDateTime.*;
-import static java.util.Map.*;
-import static org.springframework.http.HttpStatus.*;
+import static java.time.LocalDateTime.now;
+import static java.util.Map.of;
+import static org.springframework.http.HttpStatus.CREATED;
 
 @RestController
 @RequestMapping("/users")
