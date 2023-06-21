8bc84f38e87911c2f7c177ab2da6813e7cb9e1ac
Abran Lezama <lezama.abran@gmail.com>
2023-06-20 21:41:28 -0700

    Added base sql role ralated queries
    - Query to select role by name.
    - Query to assign role to user.

---
+++ src/main/java/com/abranlezama/invoicebillingsystem/role/query/RoleQuery.java
@@ -0,0 +1,13 @@
+package com.abranlezama.invoicebillingsystem.role.query;
+
+public class RoleQuery {
+    public static final String SELECT_ROLE_BY_NAME_QUERY = """
+            SELECT *
+            FROM roles
+            WHERE name = :name
+            """;
+    public static final String INSERT_ROLE_TO_USER_QUERY = """
+            INSERT INTO user_roles (user_id, role_id)
+            VALUES (:userId, :roleId)
+            """;
+}
